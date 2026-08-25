24b0340e06df7bf16e15813429012d28ea120d28
angeldavid2002 <angeldavidbolanoaroca@gmail.com>
2026-08-24 23:30:15 -0500

    Add: All the base things

---
+++ Scenes/Character/Scripts/principal_character.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@export var anim:AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	animations()
+	move_and_slide()
+	
+func animations():
+	
+	if(velocity.x<0):
+		anim.flip_h=true
+	elif(velocity.x>0):
+		anim.flip_h=false
+	
+	if(Input.is_key_pressed(KEY_X)):
+		anim.play("Attack")
+	elif (velocity.y<0):
+		anim.play("Jump")
+	elif(velocity.y>0):
+		anim.play("Fall")
+	elif(velocity.x>0):
+		anim.play("Run")
+	elif(velocity.x<0):
+		anim.play("Run")
+	elif(is_on_floor() and Input.is_action_pressed("ui_down")):
+		anim.play("Down")
+	elif(is_on_floor()):
+		anim.play("Idle")
+	
+	
